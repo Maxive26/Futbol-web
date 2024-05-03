57ab5ae06e9a03cdda12a5255ba817898240b021
Maximiliano Vera <vera.maxi2002@gmail.com>
2024-05-03 15:40:58 -0300

    Mejorando fetch de datos

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Loading from "./components/loading/loading";
+
+export default function loading() {
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
+}
