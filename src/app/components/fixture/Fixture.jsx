146d6228e353887978399936022ab096a7b4780c
Maximiliano Vera <vera.maxi2002@gmail.com>
2024-06-06 20:09:02 -0300

    Buttons query params

--- src/app/components/fixture/Fixture.jsx
+++ src/app/components/fixture/Fixture.jsx
@@ -1,25 +1,50 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FixtureLeague from "../fixtureLeague/fixtureLeague";
 import FootballBall from "../icons/footballBall";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import Row from "../icons/row";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import MatchCardSkeleton from "../matchCard/matchCardSkeleton";
 
 export default function Fixture({ data }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const valorParametro = searchParams.get("day")?.toString();
+  const valorParametro = searchParams.get("day")?.toString() || "0";
+  const [hoveredLeft, setHoveredLeft] = useState(false);
+  const [hoveredRight, setHoveredRight] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleDay = () => {
+  const handleLeftDay = () => {
+    setLoading(true);
     const params = new URLSearchParams(searchParams);
-    if (valorParametro === "1") {
-      params.set("day", 0);
-      // params.delete("day");
-    } else {
-      params.set("day", 1);
+    if (valorParametro === "0") {
+      params.set("day", "-1");
+    } else if (valorParametro === "1") {
+      params.set("day", "0");
     }
     replace(`${pathname}?${params.toString()}`);
   };
+
+  const handleRightDay = () => {
+    setLoading(true);
+    const params = new URLSearchParams(searchParams);
+    if (valorParametro === "0") {
+      params.set("day", "1");
+    } else if (valorParametro === "-1") {
+      params.set("day", "0");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  useEffect(() => {
+    setLoading(false);
+  }, [valorParametro]);
+
+  if (loading) {
+    return <MatchCardSkeleton />;
+  }
 
   return (
     <>
@@ -28,14 +53,54 @@ export default function Fixture({ data }) {
           <span className="mr-2">
             <FootballBall color={"#FFF"} />
           </span>
-          {valorParametro === "1" ? "PARTIDOS DE MAÑANA" : "PARTIDOS DE HOY"}
+          {valorParametro === "1"
+            ? "PARTIDOS DE MAÑANA"
+            : valorParametro === "-1"
+            ? "PARTIDOS DE AYER"
+            : "PARTIDOS DE HOY"}
         </h1>
-        <button
-          onClick={handleDay}
-          className="text-whiteCard font-semibold py-1 flex justify-center items-center bg-grayPage w-28 transition ease-out border-2 border-greenCard  px-3 rounded-xl hover:bg-greenCard hover:text-blackBG"
-        >
-          {valorParametro === "1" ? "Hoy" : "Mañana"}
-        </button>
+        <div className="flex gap-4">
+          <button
+            onClick={handleLeftDay}
+            onMouseEnter={() => setHoveredLeft(true)}
+            onMouseLeave={() => setHoveredLeft(false)}
+            className={`text-whiteCard rotate-180 font-semibold py-1 flex justify-center items-center bg-grayPage transition ease-out border-2 border-greenCard px-3 rounded-xl ${
+              valorParametro === "-1"
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-greenCard hover:text-blackBG"
+            }`}
+            disabled={valorParametro === "-1"}
+            data-tooltip-id="left"
+            data-tooltip-content="Partidos de ayer"
+          >
+            <Row color={hoveredLeft ? "#000" : "#EBFD69"} />
+          </button>
+          <button
+            onClick={handleRightDay}
+            onMouseEnter={() => setHoveredRight(true)}
+            onMouseLeave={() => setHoveredRight(false)}
+            className={`text-whiteCard font-semibold py-1 flex justify-center items-center bg-grayPage transition ease-out border-2 border-greenCard px-3 rounded-xl ${
+              valorParametro === "1"
+                ? "opacity-50 cursor-not-allowed"
+                : "hover:bg-greenCard hover:text-blackBG"
+            }`}
+            disabled={valorParametro === "1"}
+            data-tooltip-id="rigth"
+            data-tooltip-content="Partidos de mañana"
+          >
+            <Row color={hoveredRight ? "#000" : "#EBFD69"} />
+          </button>
+          <ReactTooltip
+            place="bottom"
+            style={{ fontSize: "12px", padding: "5px" }}
+            id="rigth"
+          />
+          <ReactTooltip
+            style={{ fontSize: "12px", padding: "5px" }}
+            place="bottom"
+            id="left"
+          />
+        </div>
       </div>
       <section className="bg-grayPage rounded-xl py-4 p-2 sm:p-5">
         {data.length === 0 ? (
